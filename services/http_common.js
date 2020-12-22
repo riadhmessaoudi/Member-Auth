e535e95755220993e6881da12661aef34f64c250
riadhmessaoudi <64573571+riadhmessaoudi@users.noreply.github.com>
2020-12-22 14:59:04 +0100

    initial commit

--- services/http_common.js
+++ services/http_common.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const API_SERVER_URL = 'http://192.168.1.4:5000' // BACK-END API URL 
 export default axios.create({
-  baseURL: "https://expressjs-jwt-auth.herokuapp.com/api",
-  // baseURL: `${API_SERVER_URL}/api`,
+   baseURL: `${API_SERVER_URL}/api`,
   headers: {
     "Content-type": "application/json"
   }
